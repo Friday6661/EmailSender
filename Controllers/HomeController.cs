4ad61dc9d1a502d7abe68d8f2c9f1c6ae79bec2a
Friday6661 <jarifr26@gmail.com>
2023-07-09 18:49:28 +0700

    first commit

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MimeKit;
+using MailKit.Net.Smtp;
+
+namespace EmailSender.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult SendEmail(string recipient, string subject, string body)
+        {
+            var message = new MimeMessage();
+            message.From.Add(new MailboxAddress("Friday", "testingfriday6661@gmail.com"));
+            message.To.Add(new MailboxAddress("", recipient));
+            message.Subject = subject;
+            message.Body = new TextPart("plain")
+            {
+                Text = body
+            };
+
+            using (var client = new SmtpClient())
+            {
+                client.Connect("sandbox.smtp.mailtrap.io", 587, false);
+                client.Authenticate("5491a7aee6a18f", "fb439a85ffbe28");
+                client.Send(message);
+                client.Disconnect(true);
+            }
+
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ Models/EmailModel.cs
@@ -0,0 +1,9 @@
+namespace EmailSender.Models
+{
+    public class EmailModel
+    {
+        public string Recipient { get; set; }
+        public string Subject { get; set; }
+        public string Body { get; set; }
+    }
+}
